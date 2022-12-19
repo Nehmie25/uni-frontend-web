8cfd46cbc1f417e38007a853025ec97bc219db62
Nehmie25 <boyernehemie@gmail.com>
2022-12-19 19:25:01 +0000

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import {  HashRouter, Route, Routes } from 'react-router-dom';
+import {  BrowserRouter, HashRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Dashboard from './pages/Dashboard';
 import Login from './pages/Login';
@@ -7,13 +7,13 @@ import Logistics from './pages/Logistics';
 function App() {
   return (
     <>
-      <HashRouter basename={process.env.PUBLIC_URL}>
+      <BrowserRouter basename={process.env.PUBLIC_URL}>
         <Routes>
           <Route path='/' element={<Login />} />
           <Route path='/dashboard' element={<Dashboard />} />
           <Route path='/logistics' element={<Logistics />} />
         </Routes>
-      </HashRouter>
+      </BrowserRouter>
     </>
   );
 }
